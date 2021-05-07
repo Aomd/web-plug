27f5ebaef0acb652b4ab8dc47bfbabff1a633f93
Aomd <g00665@163.com>
2021-05-07 10:01:22 +0800

    Update PointSimplifierManager.js
    完善销毁、初始化逻辑

--- src/PointSimplifierManager.js
+++ src/PointSimplifierManager.js
@@ -3,13 +3,18 @@ class PointSimplifierManager {
 
   constructor() {
     this.PointSimplifier = function (o) { };
-    // 用来装载 PointSimplifier 实例集合
-    this.PointSimplifiers = {
-
-    }
+    this.pointSimplifierEntity = function () { }
+    this._rightClick = function () { }
   }
 
-  _load() {
+  /**
+   * Promise 加载 misc/PointSimplifier
+   *
+   * @returns
+   * @memberof PointSimplifierManager
+   */
+  load() {
+    var _this = this;
     return new Promise(function (resolve, reject) {
       // 加载 misc/PointSimplifier
       // @ts-ignore
@@ -18,12 +23,14 @@ class PointSimplifierManager {
           reject('当前环境不支持 Canvas！')
         }
         console.info('加载 misc/PointSimplifier')
+        _this.PointSimplifier = PointSimplifier
         resolve(PointSimplifier)
       });
+
     })
   }
-  _init(type,option) {
-     var pointSimplifierEntity =  new this.PointSimplifier({
+  _init(option) {
+    var pointSimplifierEntity = new this.PointSimplifier({
       compareDataItem: function (a, b, aIndex, bIndex) {
         //数据源中靠后的元素优先，index大的排到前面去
         return aIndex > bIndex ? -1 : 1;
@@ -34,102 +41,61 @@ class PointSimplifierManager {
         return [dataItem.longitude, dataItem.latitude]
       },
       getHoverTitle: function (dataItem, idx) {
-        return dataItem.name
+        return dataItem.name || idx
       },
-      drawShadowPoint:false,
-      renderOptions: {
-        pointHoverStyle: {
-            'strokeStyle': 'rgba(0,0,0,0)',
-            'lineWidth': 0,
-            'height': 0,
-            'width': 0
-        },
-        //点的样式
-        pointStyle: {
-            // @ts-ignore
-            'content': this.PointSimplifier.Render.Canvas.getImageContent(
-              option.imgUrl,
-                function onload() {
-                  pointSimplifierEntity.renderLater();
-                },
-                function onerror(e) {
-                    console.error(`option.imgUrl:${option.imgUrl} 加载失败！`);
-                }),
-            //宽度
-            'width': 15,
-            //高度
-            'height': 15,
-            //定位点为底部中心
-            'offset': ['-50%', '-100%'],
-            'fillStyle': null,
-            'strokeStyle': null
-        },
-    },
+      renderOptions,
       ...option
     })
+
+    //  模拟海量点右键
+    // 添加 海量点 right 事件
+    // 前提 PointSimplifier 开启 pointMouseover pointMouseout
+    pointSimplifierEntity.on('pointMouseover', function (e, record) {
+      this._tempExt = record
+    })
+
+    pointSimplifierEntity.on('pointMouseout', function (e, record) {
+      this._tempExt = null;
+    })
+    this._rightClick = function (e) {
+      // 有属性 并且点击右键就出发 rightClick
+      if (pointSimplifierEntity._tempExt && e.button == 2) {
+        // 触发 rightClick 事件
+        pointSimplifierEntity.trigger('rightClick', pointSimplifierEntity._tempExt)
+      }
+    }
+    window.addEventListener('mouseup', this._rightClick)
+
+    this.pointSimplifierEntity = pointSimplifierEntity
 
     return pointSimplifierEntity
   }
   /**
    * 创建海量点
    *
-   * @param {*} type PointSimplifier 类型
    * @param {*} option 海量点构造参数配置 参考(https://lbs.amap.com/api/amap-ui/reference-amap-ui/mass-data/pointsimplifier)
    * @returns
    * @memberof PointSimplifierManager
    */
-  async create(type, option) {
-    if (!('imgUrl' in option)){
-      throw new Error('option 中 缺少重要参数 imgUrl');
-    }
-
+  async create(option) {
     if ('map' in option) {
+      // 判断是否需要重新加载
       if (this.PointSimplifier.name === 'PointSimplifier') {
-        this.PointSimplifiers[type] = this._init(type,option);
-        return this.PointSimplifiers[type]
+        return this._init(option);
       } else {
         // 加载资源
-        this.PointSimplifier = await this._load();
-        this.PointSimplifiers[type] = this._init(type,option);
-        return this.PointSimplifiers[type]
+        try {
+          this.PointSimplifier = await this.load();
+          return this._init(option);
+        } catch (error) {
+          throw new Error(error);
+        }
       }
-    }else{
+    } else {
       throw new Error('option 中 缺少重要参数 map');
     }
   }
-  /**
-   * 添加 海量点 right 事件
-   * 前提 PointSimplifier 开启 pointMouseover pointMouseout
-   *
-   * @param {string} type PointSimplifier 类型
-   * @memberof PointSimplifierManager
-   */
-  addEvent(type) {
-    if (type in this.PointSimplifiers) {
-      var PointSimplifier = this.PointSimplifiers[type];
-      PointSimplifier.on('pointMouseover', function (e, record) {
-        this._tempExt = record
-      })
 
-      PointSimplifier.on('pointMouseout', function (e, record) {
-        this._tempExt = null;
-      })
-      // 添加伪右键事件
-      window.addEventListener('mouseup', function (e) {
-
-        // 有属性 并且点击右键就出发 rightClick
-        if (PointSimplifier._tempExt && e.button == 2) {
-
-          // 触发 rightClick 事件
-          PointSimplifier.trigger('rightClick', PointSimplifier._tempExt)
-
-        }
-      })
-    } else {
-      throw new Error(`添加类型 PointSimplifier ${type} 未创建`)
-    }
-
-  }
 
   /**
    *销毁 海量点
@@ -137,23 +103,38 @@ class PointSimplifierManager {
    * @param {*} type
    * @memberof PointSimplifierManager
    */
-  destroy(type){
-    if (type in this.PointSimplifiers) {
-      this.PointSimplifiers[type].setData([])
-      this.PointSimplifiers[type] = null;
-      delete this.PointSimplifiers[type];
-      // 解绑事件
-
-
-      
-    }else{
-      throw new Error(`添加类型 PointSimplifier ${type} 未创建`)
-    }
+  destroy(type) {
+    window.removeEventListener('mouseup', this._rightClick)
+    this.pointSimplifierEntity = null;
+    this.PointSimplifier = null;
   }
 }
 
+var renderOptions = {
+  // 关闭四叉树计算密度红
+  topNAreaStyle: {
+    content: 'none',
+  },
+  //点的样式
+  pointStyle: {
+    width: 15,
+    height: 15,
+    fillStyle: '#A2D0FA'
+  },
+  // 清除外边框
+  pointHoverStyle: {
+    content: 'none',
+    width: 0,
+    height: 0,
+  },
+  //鼠标hover时的title信息
+  hoverTitleStyle: {
+    position: 'top',
+    offset: [0, '-50%']
+  }
+}
 
-
-export{
-  PointSimplifierManager
+export {
+  PointSimplifierManager,
+  renderOptions
 }
